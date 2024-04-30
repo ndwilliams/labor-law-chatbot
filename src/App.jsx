1daa4db0c8644b822f2ed9cfa95cb6f04795ef5b
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-04-29 19:21:48 -0500

    Refreshing React/jsx with Val

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+	const [count, setCount] = useState(0)
+
+	const handleBtnClick = () => {
+		console.log("clicked!")
+		setCount(count + 1)
+	}
+
+	return (
+		<>
+			<h1>Hello!</h1>
+			<div>This is Amazing!</div>
+			<button
+				className="btn-secondary"
+				onClick={() => {
+					handleBtnClick()
+				}}>
+				Click me!
+			</button>
+			<div>Count: {count}</div>
+		</>
+	)
 }
